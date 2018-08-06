dabb10d93d1a3e81f95e218a6693bcf8409711c7
Robert Davidson <r.a.davidson@live.co.uk>
2018-08-06 07:36:37 +0100

    pushing for heroku changes

--- seed/seed.dev.js
+++ seed/seed.dev.js
@@ -1,6 +1,6 @@
 const seedDB = require('./seed');
 const mongoose = require('mongoose');
-const {DB_URL} = require('../config')
+const DB_URL = process.env.DB_URL || require('../config').DB_URL
 const data = require('./devData')
 
 
