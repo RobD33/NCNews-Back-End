76680aa62510621e334302c5aa2e170f6d73cc37
Robert Davidson <r.a.davidson@live.co.uk>
2018-08-27 20:05:01 +0100

    added async map to getArticlesByTopic for comment count

--- controllers/topics.js
+++ controllers/topics.js
@@ -4,7 +4,6 @@ module.exports.getTopics = (req, res, next) => {
     Topic.find().then(topics => {
         res.status(200).send({ topics })
       }).catch(err => {
-          console.log(err)
           next(err)
       })
 }
@@ -13,20 +12,10 @@ module.exports.getArticlesByTopic = (req, res, next) => {
     const {topic_slug} = req.params;
     Article.find({belongs_to: `${topic_slug}`}).populate('created_by')
         .then(articles => {
-            let promiseArticles = articles.map(article => {
-                return Comment.countDocuments({ belongs_to: `${article._id}`})
-                        .then(commentCount => {
-                            return { ...article.toObject(), comments: commentCount}
-                        })
-            })
-            return Promise.all(promiseArticles)
-        })
-        .then(articles => {
             if (articles.length) res.send({articles})
             else res.status(404).send({msg:'no topic of that name'})
         })
         .catch(err => {
-            console.log(err)
             next(err)
         })
 }
